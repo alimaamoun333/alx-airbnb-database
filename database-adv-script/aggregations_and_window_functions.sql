41d140378cd97a075671fa04fd8adc47dbf34d3d
Ali Maamoun <alimaamoon1@gmail.com>
2025-07-05 19:52:49 +0300

    edit aggreations file

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -18,7 +18,26 @@ GROUP BY
 ORDER BY
     total_bookings DESC;
 
--- 2️⃣ Window Function: Rank properties by total number of bookings using RANK()
+-- 2️⃣ Window Function: Rank properties by total number of bookings using ROW_NUMBER()
+
+SELECT
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+FROM
+    Property p
+LEFT JOIN
+    Booking b
+ON
+    p.property_id = b.property_id
+GROUP BY
+    p.property_id,
+    p.name
+ORDER BY
+    booking_row_number;
+
+-- 3️⃣ Window Function: Rank properties by total number of bookings using RANK()
 
 SELECT
     p.property_id,

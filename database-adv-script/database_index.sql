898b4cacc462f32823e9e9f60b050ef381c78892
Ali Maamoun <alimaamoon1@gmail.com>
2025-07-05 19:59:54 +0300

    Add index creation scripts and performance measurements

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,19 +1,39 @@
--- Index on User email for faster lookup
+-- Measure query performance BEFORE adding indexes
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+WHERE user_id = 'some-uuid';
+
+-- Measure query performance BEFORE adding indexes
+EXPLAIN ANALYZE
+SELECT *
+FROM Property
+WHERE location = 'New York';
+
+-- Create indexes to optimize performance
 CREATE INDEX idx_user_email
 ON User (email);
 
--- Index on Booking user_id for faster joins/filtering
 CREATE INDEX idx_booking_user_id
 ON Booking (user_id);
 
--- Index on Booking property_id for faster joins
 CREATE INDEX idx_booking_property_id
 ON Booking (property_id);
 
--- Index on Booking start_date to optimize date range queries
 CREATE INDEX idx_booking_start_date
 ON Booking (start_date);
 
--- Index on Property location to speed up location searches
 CREATE INDEX idx_property_location
 ON Property (location);
+
+-- Measure query performance AFTER adding indexes
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+WHERE user_id = 'some-uuid';
+
+-- Measure query performance AFTER adding indexes
+EXPLAIN ANALYZE
+SELECT *
+FROM Property
+WHERE location = 'New York';
